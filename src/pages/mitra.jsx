99e1d77aad3c1bd0a6f68a43cc2eb891fe0be5dc
Imamsubekti26 <imam0subekti@gmail.com>
2024-07-21 00:57:01 +0700

    implement hooks in mitra

--- src/pages/mitra.jsx
+++ src/pages/mitra.jsx
@@ -1,35 +1,58 @@
 import DashboardLayout from "../layout/dashboardLayout";
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 export default function Mitra() {
-      return (
-        <DashboardLayout title="Mitra" tabActive="mitra">
-        <div className="card">
-          <div className="card-header">
-            <div className="d-flex align-items-center justify-content-between">
-              <h5>Daftar</h5>
-              <div className="d-flex gap-3 align-items-center">
-                <Link to="/mitra/tambah" className="btn btn-primary">Tambah</Link>
-              </div>
+  const [partnersList, setPartnersList] = useState([{}])
+
+  useEffect(() => {
+    fetchPartners()
+  }, []);
+
+  const fetchPartners = async () => {
+    setPartnersList([{id: 1, name: 'lorem' , area: 'sawah', phone: '0812', partnership_type: 'a'}])
+  }
+
+  return (
+    <DashboardLayout title="Mitra" tabActive="mitra">
+      <div className="card">
+        <div className="card-header">
+          <div className="d-flex align-items-center justify-content-between">
+            <h5>Daftar</h5>
+            <div className="d-flex gap-3 align-items-center">
+              <Link to="/mitra/tambah" className="btn btn-primary">Tambah</Link>
             </div>
           </div>
-          <div className="table-responsive text-nowrap">
-            <table className="table table-striped" id="table-data">
-              <thead>
-                <tr>
-                  <th width="15%">Nama</th>
-                  <th width="20%">Area</th>
-                  <th width="15%">Telp</th>
-                  <th width="40%">Jenis Kemitraan</th>
-                  <th width="10%">Actions</th>
-                </tr>
-              </thead>
-              <tbody className="table-border-bottom-0"></tbody>
-            </table>
-          </div>
         </div>
-        </DashboardLayout>
-      );
-    }
+        <div className="table-responsive text-nowrap">
+          <table className="table table-striped" id="table-data">
+            <thead>
+              <tr>
+                <th width="15%">Nama</th>
+                <th width="20%">Area</th>
+                <th width="15%">Telp</th>
+                <th width="40%">Jenis Kemitraan</th>
+                <th width="10%">Actions</th>
+              </tr>
+            </thead>
+            <tbody className="table-border-bottom-0">
+              {partnersList.map((partner, index) => (
+                  <tr key={index}>
+                    <td>{partner.name}</td>
+                    <td>{partner.area}</td>
+                    <td>{partner.phone}</td>
+                    <td>{partner.partnership_type}</td>
+                    <td>
+                      <Link to={`/mitra/${partner.id}`} className="btn btn-sm btn-secondary">Edit</Link>
+                    </td>
+                  </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
+}
   
 
--- src/pages/tambahMitra.jsx
+++ src/pages/tambahMitra.jsx
@@ -1,16 +1,46 @@
 import DashboardLayout from "../layout/dashboardLayout";
-import {Link} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 export default function TambahMitra() {
+  const id = useParams().id;
+  const currentMode = id ? 'Edit' : 'Tambah';
+
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [area, setArea] = useState('');
+  const [phone, setPhone] = useState('');
+  const [partnershipType, setPartnershipType] = useState('');
+
+  useEffect(() => {
+    if (id) {
+      fetchSingleData(id);
+    }
+  }, [id]);
+
+  const fetchSingleData = id => {
+    // TODO: fetch single data by id
+    console.log(id);
+  }
+  const handleSubmit = event => {
+    event.preventDefault();
+    // TODO: when form is submit
+  }
+  const handleDelete = () => {
+    // TODO: handle delete
+  }
+
   return (
     <DashboardLayout title="Mitra" tabActive="mitra">
       <div className="card mb-4">
         <div className="card-header d-flex align-items-center justify-content-between">
-          <h5 className="mb-0"></h5>
-          <button className="btn btn-sm btn-outline-danger" id="btn-delete">Hapus Data Ini</button>
+          <h5 className="mb-0">{currentMode} Mitra</h5>
+          {id && (
+            <button className="btn btn-sm btn-outline-danger" id="btn-delete" onClick={handleDelete}>Hapus Data Ini</button>
+          )}
         </div>
         <div className="card-body">
-          <form id="form-data">
+          <form id="form-data" onSubmit={handleSubmit}>
             <div className="row mb-3">
               <label className="col-sm-2 col-form-label" htmlFor="name">
                 Nama Mitra
@@ -21,8 +51,8 @@ export default function TambahMitra() {
                   className="form-control"
                   id="name"
                   placeholder="ex: pak ard..."
-                  name="name"
-                  value="name"
+                  value={name}
+                  onChange={e => setName(e.target.value)}
                 />
               </div>
             </div>
@@ -36,8 +66,8 @@ export default function TambahMitra() {
                   className="form-control"
                   id="address"
                   placeholder="alamat lengkap"
-                  name="address"
-                  value="address"
+                  value={address}
+                  onChange={e => setAddress(e.target.value)}
                 />
               </div>
             </div>
@@ -51,8 +81,8 @@ export default function TambahMitra() {
                   className="form-control"
                   id="area"
                   placeholder="kecamatan, kabupaten"
-                  name="area"
-                  value="area"
+                  value={area}
+                  onChange={e => setArea(e.target.value)}
                 />
               </div>
             </div>
@@ -66,8 +96,8 @@ export default function TambahMitra() {
                   className="form-control"
                   id="phone"
                   placeholder="ex: 08123..."
-                  name="phone"
-                  value="phone"
+                  value={phone}
+                  onChange={e => setPhone(e.target.value)}
                 />
               </div>
             </div>
@@ -81,14 +111,14 @@ export default function TambahMitra() {
                   className="form-control"
                   id="partnership_type"
                   placeholder="ex: pembesaran puyuh, petelur puyuh..."
-                  name="partnership_type"
-                  value="partnership_type"
+                  value={partnershipType}
+                  onChange={e => setPartnershipType(e.target.value)}
                 />
               </div>
             </div>
             <div className="row justify-content-end">
               <div className="col-sm-10">
-                <button type="submit" className="btn btn-primary" id="btn-submit"></button>
+                <button type="submit" className="btn btn-primary me-2" id="btn-submit">{currentMode}</button>
                 <Link to="/mitra" type="button" className="btn btn-outline-secondary">kembali</Link>
               </div>
             </div>
